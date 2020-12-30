41042033e36ee735d7e4fc9133270d996f8b286a
Ali Watters <ali.watters@gmail.com>
2020-12-29 20:00:05 -0800

    wip

---
+++ client/src/Fib.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Fib extends Component {
+  state = {
+    seenIndexes: [],
+    values: {},
+    index: ''
+  };
+
+  componentDidMount() {
+    this.fetchValues();
+    this.fetchIndexes();
+  }
+
+  async fetchValues() {
+    const values = await axios.get('/api/values/current');
+    this.setState({ values: values.data });
+  }
+
+  async fetchIndexes() {
+    const indexes = await axios.get('/api/values/all');
+    this.setState({ seenIndexes: indexes.data });
+  }
+
+  render() {
+    return (
+      <form>
+        <label>Enter your index</label>
+      </form>
+    );
+  }
+}
