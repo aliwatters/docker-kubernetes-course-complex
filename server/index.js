169e314174a9ba935aa51ab3d36c4f1b812fc115
Ali Watters <ali.watters@gmail.com>
2021-01-01 20:25:27 -0800

    updated title, fixed db bug

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ app.use(bodyParser.json());
 const { Pool } = require('pg');
 const pgClient = new Pool({
   user: keys.pgUser,
-  host: keys.pgDatabase,
+  database: keys.pgDatabase,
+  host: keys.pgHost,
   password: keys.pgPassword,
   port: keys.pgPort
 });
